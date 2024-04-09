d5c6a4b2132d49468344d4bf6455edcf38ffbce6
shantokarmoker8 <shantokarmoker8@gmail.com>
2024-04-09 23:39:42 +0600

    react icon

--- src/pages/Home/NewsCard.jsx
+++ src/pages/Home/NewsCard.jsx
@@ -1,5 +1,32 @@
-const NewsCard = (aNews) => {
-  return <div>news</div>;
-};
+import PropTypes from "prop-types";
+import { IoShareSocialOutline } from "react-icons/io5";
 
+import { FaRegBookmark } from "react-icons/fa6";
+const NewsCard = ({ aNews }) => {
+  const { author, title } = aNews;
+  return (
+    <div>
+      <div>
+        <div className="flex justify-between items-center p-5 bg-[#F3F3F3]">
+          <div className="flex gap-4 ">
+            <div>
+              <img src={author.img} alt="" className="w-10 rounded-full" />
+            </div>
+            <div>
+              <h2 className="text-base font-semibold">{author.name}</h2>
+              <p className="text-sm text-[#706F6F]">{author.published_date}</p>
+            </div>
+          </div>
+          <div>
+            <FaRegBookmark></FaRegBookmark>
+            <IoShareSocialOutline></IoShareSocialOutline>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+NewsCard.propTypes = {
+  aNews: PropTypes.object,
+};
 export default NewsCard;
